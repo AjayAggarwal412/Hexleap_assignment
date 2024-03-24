32f011d0eabc9e852d93a57f589f0b5a0977bbc7
Ajay Aggarwal <iaminhurry123@gmail.com>
2024-03-24 11:52:30 +0530

    fifth commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import * as React from "react";
 import Custom from "./card";
-import theme from "./lib/theme";
+import theme from "../styles/theme";
 
 export default function Home() {
   return (
